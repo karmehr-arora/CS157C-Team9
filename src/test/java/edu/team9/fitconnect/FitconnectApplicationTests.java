a707ef27a045a99ea3405c6bfb0aa92f5b07e8c8
Constantine Khamis <costikhamis@gmail.com>
2024-03-16 22:01:50 -0700

    gradle dependency

---
+++ src/test/java/edu/team9/fitconnect/FitconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.team9.fitconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitconnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
