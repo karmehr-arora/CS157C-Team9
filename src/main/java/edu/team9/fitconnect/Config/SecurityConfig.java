5bc4d0a69b4e9688286382613c2613c0915eeeb5
Constantine Khamis <costikhamis@gmail.com>
2024-03-16 19:48:11 -0700

    Changed application stack to Java Spring

---
+++ src/main/java/edu/team9/fitconnect/Config/SecurityConfig.java
@@ -0,0 +1,19 @@
+package edu.team9.fitconnect.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain web(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authorize) ->
+                        authorize.anyRequest().permitAll()
+                );
+        return http.build();
+    }
+
+}
---
+++ src/main/java/edu/team9/fitconnect/FitconnectApplication.java
@@ -0,0 +1,13 @@
+package edu.team9.fitconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitconnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitconnectApplication.class, args);
+	}
+
+}
