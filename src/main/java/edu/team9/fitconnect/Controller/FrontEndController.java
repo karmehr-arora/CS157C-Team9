6cc1cf151571e9c06a6bbb3c5b7d23ab68d5eaf3
karmehr-arora <karmehr.arora@gmail.com>
2024-05-02 01:58:05 -0700

    Checks that email is unique before creating an account

--- src/main/java/edu/team9/fitconnect/Controller/FrontEndController.java
+++ src/main/java/edu/team9/fitconnect/Controller/FrontEndController.java
@@ -49,10 +49,12 @@ public class FrontEndController {
     @PostMapping(value = "/newaccount")
     public String createAccount(@RequestParam String firstName, @RequestParam String lastName, @RequestParam String email, @RequestParam String displayName, @RequestParam String password, @RequestParam String confirmPassword, @RequestParam int weight, @RequestParam int heightInInches){
         // if email doesn't already exist in the database, then do the following:
-        if(password.equals(confirmPassword)){
-            userService.createUser(email, LocalDateTime.now(), displayName, null, firstName, weight, heightInInches, lastName, password, null, null, User.Role.USER, weight);
-            return "main/login";
-        }
+        if(!(email.equals(userService.loadUserByUsername(email).getUsername()))){
+            if(password.equals(confirmPassword)){
+                userService.createUser(email, LocalDateTime.now(), displayName, null, firstName, weight, heightInInches, lastName, password, null, null, User.Role.USER, weight);
+                return "main/login";
+            }
+            }
         return "main/signup";
     }
 
